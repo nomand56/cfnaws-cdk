c64b5c08089321b0a5b5064fc8fd551183417284
nomand56 <nomandogar56@gmail.com>
2022-09-12 15:53:24 +0500

    changed
    sad

--- lib/cfnaws-stack.ts
+++ lib/cfnaws-stack.ts
@@ -1,5 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { aws_cloudfront as cloudfront } from "aws-cdk-lib";
+import { aws_cloudfront_origins as origins } from "aws-cdk-lib";
+import { aws_s3_deployment as s3deploy } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CfnawsStack extends cdk.Stack {
@@ -8,9 +12,27 @@ export class CfnawsStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CfnawsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Define Bucket first
+    const bucket = new s3.Bucket(this, "MyFirstBucket");
+
+    //Adding CDN File
+    const distribution = new cloudfront.Distribution(this, "myDist", {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+      defaultRootObject: "index.html",
+    });
+
+    //printing outputs for cloud front
+    new cdk.CfnOutput(this, "DistributionDomain", {
+      value: distribution.domainName,
+    });
+
+    //
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./website")],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
